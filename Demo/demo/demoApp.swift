a019d01341aa551b80cc069c7005627f4e0e8fbb
yogita109 <ush109rock@gmail.com>
2021-09-02 16:43:24 +0900

    Change project structure

---
+++ Demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by uematsushun on 2021/09/02.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
